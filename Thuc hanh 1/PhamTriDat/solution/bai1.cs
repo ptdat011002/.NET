bca75df55ae266e7f96cf3216e56b5525f29150c
ptdat011002 <phamtridat01@gmail.com>
2022-03-22 20:23:04 +0700

    1

---
+++ Thuc hanh 1/PhamTriDat/solution/bai1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhamTriDat.solution.b1
+{
+    class bai1
+    {
+        public void tamgiac()
+        {
+            Console.WriteLine("nhap chieu cao h: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+                for(int i=1; i <=h; i++)
+                {
+                    for(int j=1; j<=h; j++)
+                    {
+                        if(j<= h - i)
+                        {
+                            Console.Write("   ");
+                        }
+                        else
+                        {
+                            Console.Write("  *");
+                        }
+                    }
+                    Console.WriteLine("\n");
+                }          
+                Console.WriteLine();
+        }
+    }
+}
---
+++ Thuc hanh 1/PhamTriDat/solution/bai4(sinhvien).cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhamTriDat.solution.b4
+{
+    public class sinhvien
+    {
+        private int masv;
+        private String tensv;
+        private int soluongTC;
+        private tinchi[] TC;
+        public sinhvien() { }
+        public void nhapsv()
+        {
+            Console.Write("ma sinh vien: ");
+            soluongTC = int.Parse(Console.ReadLine());
+            Console.Write("ten sinh vien: ");
+            tensv = Console.ReadLine();
+            Console.Write("so luong mon da dang ki: ");
+            soluongTC = int.Parse(Console.ReadLine());
+            TC = new tinchi[soluongTC];
+            for (int i = 0; i < soluongTC; i++)
+            {
+                TC[i] = new tinchi();
+                TC[i].nhap();
+            }
+        }
+        public float TongKet()
+        {
+            float kq = 0;
+            int STC = 0;
+            for (int i = 0; i < soluongTC; i++)
+            {
+                kq = kq + TC[i].GetDTB() * TC[i].GetSTC();
+                STC = STC + TC[i].GetSTC();
+            }
+            if (STC != 0)
+            {
+                kq = kq / STC;
+            }
+            return kq;
+        }
+        public void XepLoaiSV()
+        {
+            if (TongKet() < 6)
+            {
+                Console.WriteLine("xep loai trung binh");
+            }
+            else if (TongKet() < 8 && TongKet() >= 6)
+            {
+                Console.WriteLine("xep loai kha");
+            }
+            else
+            {
+                Console.WriteLine("xep loai gioi");
+            }
+        }
+        public void xuatsv()
+        {
+            Console.WriteLine($"{masv}\t {tensv}\t {soluongTC}\t");
+            for (int i = 0; i < soluongTC; i++)
+            {
+                TC[i].xuat();
+            }
+            Console.WriteLine(TongKet());
+            XepLoaiSV();
+        }
+    }
+}
