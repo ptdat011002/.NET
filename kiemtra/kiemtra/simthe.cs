27a64c1d9c7b3e235f4a0e32b36ceedeaabf22aa
ptdat011002 <phamtridat01@gmail.com>
2022-04-12 12:53:25 +0700

    2

---
+++ kiemtra/kiemtra/simthe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiemtra
+{
+    class simthe
+    {
+        private string _id;
+        private string _sosim;
+        private string _mangid;
+        private DateTime ngaykichhoat;
+        private DateTime ngayhethan;
+
+        public string Id { get => _id; set => _id = value; }
+        public string Sosim { get => _sosim; set => _sosim = value; }
+        public string Mangid { get => _mangid; set => _mangid = value; }
+        public DateTime Ngaykichhoat { get => ngaykichhoat; set => ngaykichhoat = value; }
+        public DateTime Ngayhethan { get => ngayhethan; set => ngayhethan = value; }
+
+        public simthe() { }
+    }
+}
