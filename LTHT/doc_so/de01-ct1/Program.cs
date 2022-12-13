698852026471a7e260a73146b3ce2a41f631366c
ptdat011002 <phamtridat01@gmail.com>
2022-12-13 14:26:07 +0700

    c#

---
+++ LTHT/doc_so/de01-ct1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace de01_ct1
+{
+    internal class Program
+    {
+        public static void ct1()
+        {
+            var rd = new Random();
+            while (true)
+            {
+                try
+                {
+                    
+                    var num = rd.Next(0,int.MaxValue);
+                    Console.WriteLine(num);
+                    File.WriteAllText("D:\\de01.txt", num.ToString());
+                    if (num % 1024 == 0) break;
+                }
+                catch(Exception)
+                { }
+            }
+        }
+        public static void ct2()
+        {
+            while (true)
+            {
+                try
+                {
+                    var s = File.ReadAllText("D:\\de01.txt");
+                    Console.WriteLine(s);
+                    int a = int.Parse(s);
+                    if (a % 1024 == 0) break;
+                }
+                catch (Exception)
+                { }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(ct1);
+            t1.Start();
+            Thread t2 = new Thread(ct2);
+            t2.Start();
+        }
+    }
+}
---
+++ LTHT/timer/de07/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Media;
+namespace de07
+{
+    internal class Program
+    {
+        public static string c;
+        public static bool check_changed = false;
+        public static void timer1()
+        {
+            while(true)
+            {
+                try
+                {
+                    Console.Write("a : ");
+                    string a = Console.ReadLine();
+                    c = a;
+                    Console.WriteLine("a = " + c);
+                    
+                    if (c == "!") break;
+                    Thread.Sleep(7);
+                }
+                catch(Exception)
+                { }
+            }
+        }
+        public static void timer2()
+        {
+            while(true)
+            {
+                try
+                {                  
+                        Console.Beep();
+                        if (c == "!") break;
+                        Thread.Sleep(10);
+                }
+                catch(Exception)
+                { }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(timer1);
+            t1.Start();
+            Thread t2 = new Thread(timer2);
+            t2.Start();
+        }
+    }
+}
